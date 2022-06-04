5c1cd6bfa318d2563e0d300d3f66a8c83fba6513
theoboldalex <theoboldalex@gmail.com>
2022-06-04 22:16:11 +0100

    Spit PHPUnit results into floating window buffer

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,1 +1,27 @@
-print("Ahoy!")
+local test_result = function()
+    local lines = {}
+    local cmd = vim.api.nvim_exec("!./vendor/bin/phpunit", true)
+    
+    for line in cmd:gmatch("[^\r\n]+") do
+        table.insert(lines, line)
+    end
+
+    return lines
+end
+
+local buffer = vim.api.nvim_create_buf(false, true)
+
+local options = {
+  relative = 'win',
+  width = 120,
+  height = 20,
+  style = "minimal",
+  win = 1001,
+  row = 20,
+  col = 20
+}
+
+vim.api.nvim_open_win(buffer, true, options)
+
+vim.api.nvim_buf_set_lines(buffer, 2, 2, false, test_result())
+
