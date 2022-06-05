695596aa47bddd11f3c68365bd0f4e40b65705ba
theoboldalex <theoboldalex@gmail.com>
2022-06-05 20:58:40 +0100

    Commented mapping for faster iteration on test

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,1 +1,4 @@
 vim.cmd([[command! PUnit :lua require"phpunit".run_tests()]])
+
+-- Just for my own testing. Users can choose teir own mapping
+--vim.api.nvim_set_keymap("n", "<leader>pu", "<cmd>PUnit<CR>", {})
